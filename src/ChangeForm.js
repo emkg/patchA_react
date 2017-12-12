e71f065f2dc1c709c45a88008db20bea92fab238
emkg <emilykathryngrimes@gmail.com>
2017-12-12 17:44:55 -0600

    works with the api, to the point that I have hit a limit on what the api can do

--- src/ChangeForm.js
+++ src/ChangeForm.js
@@ -1,42 +1,97 @@
 import React from 'react';
 import './ChangeForm.css';
+import { Redirect } from 'react-router-dom';
 
 let title = "Change Form";
-const stop = (e) => (e.stopPropagation(), e.preventDefault());
+
+const stop = (event) => (event.stopPropagation(), event.preventDefault());
+
 export default class ChangeForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      whatwhy: 'update all the computers because they need it',
+      how: 'reboot and install',
+      duration: 'forever',
+      software: '',
+      user: 'Your name:',
+      email: 'Your noaa.gov email:',
+      exception: 0,
+      success: false,
+      redirect: '/'
+    };
 
-  handleSubmit = async e => {
-    stop(e);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    const data = new FormData(e.target);
+  handleChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit = async event => {
+    stop(event);
+
+    const data = this.state;
+    this.setState({ success: !this.state.success })
+    console.log(data);
     await fetch('/api/addTicket.php', {
       'method' : 'post',
       'body' : JSON.stringify(data)
     });
   }
   render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <h3>{title}</h3>
-          <p>What are the details of this request? Why should this change be implemented?
-          Please include the consequences of not implementing this change.</p>
-          <textarea type='text' rows='5' name='whatwhy'></textarea>
-          <p>Provide a suggested implementation plan including contingency (rollback) plans.</p>
-          <textarea type='text' name='how' rows='5'></textarea>
-          <p>What is the timeframe for this change?</p>
-          <textarea type='text' rows='2' name='duration'></textarea>
-          <p>What software systems will be affected (if known)?</p>
-          <select name='software'>
-            <option value="Mac">Mac</option>
-            <option value="Windows">Windows</option>
-            <option value="Linux">Linux</option>
-          </select>
-          <input type='text' name='user'  placeholder='Your name:'/>
-          <input type='text' name='email' placeholder='Your email:' required pattern='\(@noaa.gov)$' />
-          <button type='submit'><span>SUBMIT</span></button>
-        </form>
-      </div>
-    );
+    if(!this.state.success) {
+      return (
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <h3>{title}</h3>
+            <p>What are the details of this request? Why should this change be implemented?
+            Please include the consequences of not implementing this change.</p>
+            <textarea type='text'
+                      rows='5'
+                      name='whatwhy'
+                      value={this.state.whatwhy}
+                      onChange={this.handleChange}></textarea>
+            <p>Provide a suggested implementation plan including contingency (rollback) plans.</p>
+            <textarea type='text'
+                      name='how'
+                      rows='5'
+                      value={this.state.how}
+                      onChange={this.handleChange}></textarea>
+            <p>What is the timeframe for this change?</p>
+            <textarea type='text'
+                      rows='2'
+                      name='duration'
+                      value={this.state.duration}
+                      onChange={this.handleChange}></textarea>
+            <p>What software systems will be affected (if known)?</p>
+            <select name='software' value={this.state.software} onChange={this.handleChange}>
+              <option value="Mac">Mac</option>
+              <option value="Windows">Windows</option>
+              <option value="Linux">Linux</option>
+            </select>
+            <input type='text'
+                   name='user'
+                   value={this.state.user}
+                   onChange={this.handleChange}/>
+            <input type='email'
+                   name='email'
+                   required
+                   value={this.state.email}
+                   onChange={this.handleChange} />
+            <button type='submit'><span>SUBMIT</span></button>
+          </form>
+        </div>
+      );
+    } else {
+      return ( <Redirect push to={this.state.redirect} />)
+    }
   }
 }
--- src/ExceptionForm.js
+++ src/ExceptionForm.js
@@ -2,29 +2,74 @@ import React from 'react';
 import './ChangeForm.css';
 import {
   BrowserRouter as Router,
+  Route,
   Redirect,
 } from 'react-router-dom';
 
 export default class ExceptionForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      software: 'update all the computers because they need it',
+      reason: 'reboot and install',
+      user: 'Your name:',
+      email: 'Your noaa.gov email:',
+      exception: 1,
+      succuss: false,
+      redirect: '/'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit = async event => {
+    // stop event
+    event.stopPropagation();
+    event.preventDefault();
+
+    this.setState({ success: !this.state.submitted });
+
+    const data = this.state;
+    console.log(data);
+    await fetch('/api/addTicket.php', {
+      'method' : 'post',
+      'body' : JSON.stringify(data)
+    });
+  }
 
   render() {
-    return (
-      <div>
-        <form name='exception'>
-        	<h3>Request to reschedule this update on a particular system.</h3>
-          <p>What is the OS of the server?</p>
-          <select name="os">
-            <option value='Windows'>Windows</option>
-            <option value='Mac'>Mac</option>
-            <option value='Linux'>Linux</option>
-          </select>
-        	<p>Please provide a reason why you need this exception: </p>
-        	<textarea type='text' name='reason' rows='4'></textarea>
-        	<input type='text' name='user' placeholder='Your name:'/>
-        	<input type='text' name='email' placeholder='Your email:' required pattern='\(@noaa.gov)$'/>
-          <button><span>SUBMIT</span></button>
-        </form>
-      </div>
-    );
+      if(!this.state.success) {
+        return (
+          <div>
+            <form name='exception' onSubmit={this.handleSubmit}>
+            	<h3>Request to reschedule this update on a particular system.</h3>
+              <p>What is the OS of the server?</p>
+              <select name="software" value={this.state.software} onChange={this.handleChange}>
+                <option value='Windows'>Windows</option>
+                <option value='Mac'>Mac</option>
+                <option value='Linux'>Linux</option>
+              </select>
+            	<p>Please provide a reason why you need this exception: </p>
+            	<textarea type='text' name='reason' value={this.state.reason} onChange={this.handleChange} rows='4'></textarea>
+            	<input type='text' name='user' value={this.state.user} onChange={this.handleChange} />
+            	<input type='email' name='email' value={this.state.email} onChange={this.handleChange} required />
+              <button><span>SUBMIT</span></button>
+            </form>
+          </div>
+        );
+      } else {
+        return ( <Redirect push to={this.state.redirect} />);
+      }
   }
 }
